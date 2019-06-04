7d1befaf503a6a17f8768324351a6c444ebb5aff
joseivson <joseivson@gmail.com>
2019-06-04 09:26:30 -0300

    fix variable tripletnet; windows batch

---
+++ runs/train.bat
@@ -0,0 +1,68 @@
+#!Windows batch
+
+# parameters
+DATA=%CD%/.datasets
+NAMEDATASET='mnist'
+PROJECT='../out/tripletruns'
+EPOCHS=150
+BATCHSIZETRAIN=60
+BATCHSIZETEST=60
+LEARNING_RATE=0.0001
+MOMENTUM=0.5
+WEIGHT_DECAY=0.0005
+PRINT_FREQ=50
+WORKERS=10
+RESUME='chk000000xxx.pth.tar'
+GPU=0
+ARCH='preactresembnet18'
+LOSS='hinge'
+OPT='adam'
+SCHEDULER='fixed'
+SNAPSHOT=10
+NUMCHANNELS=3
+NUMCLASS=10
+MARGIN=1
+TRAINSIZE=10000
+VALSIZE=5000
+DIM=32
+IMAGESIZE=32
+EXP_NAME='triplet_'%ARCH%'_'%LOSS%'_'%OPT%'_'%NAMEDATASET%'_emb'%DIM%'_imsize'%IMAGESIZE%'_000'
+
+
+rm -rf %PROJECT%/%EXP_NAME%/%EXP_NAME%.log
+rm -rf %PROJECT%/%EXP_NAME%/
+mkdir %PROJECT%    
+mkdir %PROJECT%/%EXP_NAME%  
+
+
+## execute
+CUDA_VISIBLE_DEVICES=3  python ../train.py \
+%DATA% \
+--project=%PROJECT% \
+--name=%EXP_NAME% \
+--epochs=%EPOCHS% \
+--batch-size-train=%BATCHSIZETRAIN% \
+--batch-size-test=%BATCHSIZETEST% \
+--image-size=%IMAGESIZE% \
+--learning-rate=%LEARNING_RATE% \
+--momentum=%MOMENTUM% \
+--print-freq=%PRINT_FREQ% \
+--workers=%WORKERS% \
+--resume=%RESUME% \
+--gpu=%GPU% \
+--loss=%LOSS% \
+--opt=%OPT% \
+--snapshot=%SNAPSHOT% \
+--scheduler=%SCHEDULER% \
+--arch=%ARCH% \
+--margin=%MARGIN% \
+--dim=%DIM% \
+--num-classes=%NUMCLASS% \
+--triplet-size-train=%TRAINSIZE% \
+--triplet-size-val=%VALSIZE% \
+--name-dataset=%NAMEDATASET% \
+--channels=%NUMCHANNELS% \
+--finetuning \
+--parallel \
+2>&1 | tee -a %PROJECT%/%EXP_NAME%/%EXP_NAME%.log \
+
--- torchlib/tripletnet.py
+++ torchlib/tripletnet.py
@@ -5,12 +5,12 @@ import torch.nn.functional as F
 class Tripletnet(nn.Module):
     def __init__(self, embeddingnet):
         super(Tripletnet, self).__init__()
-        self.embeddingnet = embeddingnet
+        self.module = embeddingnet
 
     def forward(self, anchor, positive, negative):
-        embedded_a = self.embeddingnet(anchor)
-        embedded_p = self.embeddingnet(positive)
-        embedded_n = self.embeddingnet(negative)
+        embedded_a = self.module(anchor)
+        embedded_p = self.module(positive)
+        embedded_n = self.module(negative)
         return embedded_a, embedded_p, embedded_n
 
 
